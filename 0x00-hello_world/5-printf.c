5f7090509909a12517cd95f5343f7c36b4a673ed
IsraelKenneth <israel.kenneth2011@gmail.com>
2023-02-16 05:53:58 -0800

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description - this uses printf
+ *Return: return an integer
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
