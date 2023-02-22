9cc65bb1775b22329952d500df9ce1c3420fbd5d
IsraelKenneth <israel.kenneth2011@gmail.com>
2023-02-22 05:07:16 -0800

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,33 +1,7 @@
-#include <unistd.h>
+#include <stdio.h>
 
 int main(void)
 {
-    char c = '_';
-    write(1, &c, 1);
-
-    c = 'p';
-    write(1, &c, 1);
-
-    c = 'u';
-    write(1, &c, 1);
-
-    c = 't';
-    write(1, &c, 1);
-
-    c = 'c';
-    write(1, &c, 1);
-
-    c = 'h';
-    write(1, &c, 1);
-
-    c = 'a';
-    write(1, &c, 1);
-
-    c = 'r';
-    write(1, &c, 1);
-
-    c = '\n';
-    write(1, &c, 1);
-
-    return (0);
-}}
+    printf("_putchar\n");
+    return 0;
+}
